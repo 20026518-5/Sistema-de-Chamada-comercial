896e86f4c0526a929f17e93319748e2ec7cffea5
Lucas Vinicius / INTELLEART <147888270+20026518-5@users.noreply.github.com>
2026-01-04 10:34:30 -0400

    Update index.js

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,44 +1,67 @@
-
-import { FiX } from 'react-icons/fi';
 import './modal.css';
+import { FiX } from 'react-icons/fi';
 
-export default function Modal({conteudo,buttomBack}){
- 
-  return (
-    <div className='modal'>
-      <div className='container'>
-        <button className='btn-back' onClick={buttomBack}>
-          <FiX size={25} color='#fff' />
+export default function Modal({ conteudo, buttomBack }){
+  return(
+    <div className="modal">
+      <div className="container">
+        <button className="close" onClick={ buttomBack }>
+          <FiX size={25} color="#FFF" />
           Voltar
         </button>
-      
 
-      <main>
-      <h2>Detalhes do Chamado:</h2>
-      <div className='details'>
-        
-          <span>Cliente:<i>{conteudo.cliente}</i> </span>
-        </div>
-        <div className='details'>
-          <span>Assunto:<i>{conteudo.assunto}</i> </span>
-          <span>Cadastrado em:<i>{conteudo.createdFormat}</i> </span>
-        </div>
-        <div className='details'>
-          <span>Status:<i className='badge' style={{color:'#fff',backgroundColor:conteudo.status==='Em aberto'?'#5cb85c':'#999'}}>
-            {conteudo.status}
-            </i> </span>
-        </div>
-        <div className='details'>
-        
-        {conteudo.complemento.length !== 0 && (
-          <div>
-          <span>Complemento</span>
-          <p>{conteudo.complemento}</p>
+        <main>
+          <h2>Detalhes do chamado</h2>
+
+          <div className="row">
+            <span>
+              Cliente: <i>{conteudo.cliente}</i>
+            </span>
           </div>
-        )}
-          
-        </div>
-      </main>
+
+          <div className="row">
+            <span>
+              Assunto: <i>{conteudo.assunto}</i>
+            </span>
+            <span>
+              Cadastrado em: <i>{conteudo.createdFormat}</i>
+            </span>
+          </div>
+
+          <div className="row">
+            <span>
+              Status: 
+              <i className="status-badge" style={{ color: "#FFF", backgroundColor: conteudo.status === 'Em aberto' ? '#5CB85C' : '#999' }}>
+                {conteudo.status}
+              </i>
+            </span>
+          </div>
+
+          {conteudo.complemento !== '' && (
+            <>
+              <h3>Complemento</h3>
+              <p>
+                {conteudo.complemento}
+              </p>
+            </>
+          )}
+
+          {/* SECÇÃO DE AUDITORIA: Exibe apenas se houver atualização */}
+          {conteudo.updatedBy && (
+            <div className="audit-info">
+              <hr />
+              <h4>Histórico de Atualização</h4>
+              <span>
+                Última alteração por: <strong>{conteudo.updatedBy}</strong>
+              </span>
+              <br />
+              <span>
+                Data da alteração: <i>{conteudo.updatedFormat}</i>
+              </span>
+            </div>
+          )}
+
+        </main>
       </div>
     </div>
   )
