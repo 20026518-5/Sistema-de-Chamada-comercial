a6df6552757465a030d24a5985d7df9ae3f6246e
Lucas Vinicius / INTELLEART <147888270+20026518-5@users.noreply.github.com>
2026-01-03 21:19:18 -0400

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import avatarImg from '../../assets/avatar.png';   
 import { AuthContext } from '../../contexts/auth';
-import { FiHome, FiSettings, FiUser, FiLogOut, FiUsers } from 'react-icons/fi'; // Importação corrigida
+import { FiHome, FiSettings, FiUser, FiLogOut, FiUsers } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import './header.css';
 
@@ -14,36 +14,32 @@ export default function Header() {
         <img src={user.avatarUrl === null ? avatarImg : user.avatarUrl} alt='foto do usuario' />
       </div>
       
-      {/* 1. Chamados */}
       <Link to='/dashboard'>
         <FiHome color='#fff' size={24} />
         Chamados
       </Link>
 
-      {/* 2. Servidores (Ajustado conforme sua solicitação anterior) */}
       <Link to='/servidores'>
         <FiUsers color='#fff' size={24} />
         Servidores
       </Link>
 
-      {/* 3. Perfil (Usando ícone de usuário para evitar confusão) */}
       <Link to='/profile'>
         <FiUser color='#fff' size={24} />
         Perfil
       </Link>
 
-      {/* 4. Configurações de Setores (Apenas para Admin) */}
+      {/* Nome alterado de 'Configurações' para 'Secretarias' */}
       {user.isadm && (
         <Link to='/settings'>
           <FiSettings color='#fff' size={24} />
-          Configurações
+          Secretarias
         </Link>
       )}
 
-      {/* 5. Botão de Sair */}
-      <button onClick={() => logOut()} className="logout-btn" style={{ background: 'transparent', border: 0, display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
+      <button onClick={() => logOut()} className="logout-btn">
         <FiLogOut color='#fff' size={24} />
-        <span style={{ color: '#FFF', marginLeft: '0.7em', fontSize: '1.1em' }}>Sair</span>
+        <span>Sair</span>
       </button>
     </div>
   )
