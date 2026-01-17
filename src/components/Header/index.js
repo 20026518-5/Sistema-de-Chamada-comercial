c4363a069984dd1b3d7335137a5373cd42cb8b38
Lucas Vinicius / INTELLEART <147888270+20026518-5@users.noreply.github.com>
2026-01-17 16:04:17 -0400

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,46 @@
 import { useContext } from 'react';
-import './header.css';
+import avatarImg from '../../assets/avatar.png';   
 import { AuthContext } from '../../contexts/auth';
-import avatar from '../../assets/avatar.png';
+import { FiHome, FiSettings, FiUser, FiLogOut, FiUsers } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { FiHome, FiUser, FiSettings } from "react-icons/fi";
+import './header.css';
 
-export default function Header(){
-  const { user } = useContext(AuthContext);
+export default function Header() {
+  const { user, logout } = useContext(AuthContext); 
 
-  return(
-    <div className="sidebar">
+  return (
+    <div className='sidebar'>
       <div>
-        <img src={user.avatarUrl === null ? avatar : user.avatarUrl} alt="Foto avatar" />
+        <img src={user.avatarUrl === null ? avatarImg : user.avatarUrl} alt='foto do usuario' />
       </div>
-
-      <Link to="/dashboard">
-        <FiHome color="#FFF" size={24} />
+      
+      <Link to='/dashboard'>
+        <FiHome color='#fff' size={24} />
         Chamados
       </Link>
 
-      <Link to="/customers">
-        <FiUser color="#FFF" size={24} />
+      <Link to='/customers'>
+        <FiUsers color='#fff' size={24} />
         Clientes
       </Link>
 
-      <Link to="/profile">
-        <FiSettings color="#FFF" size={24} />
-        Configurações
+      <Link to='/profile'>
+        <FiUser color='#fff' size={24} />
+        Perfil
       </Link>
+
+      {user.isadm && (
+        <Link to='/settings'>
+          <FiSettings color='#fff' size={24} />
+          Secretarias
+        </Link>
+      )}
+
+      {/* Classe renomeada para evitar conflito com o CSS do Perfil */}
+      <button onClick={ () => logout() } className="sidebar-logout">
+        <FiLogOut color='#fff' size={24} />
+        <span>Sair</span>
+      </button>
     </div>
   )
 }
