61f1d8d20b41bd0e1e306e1dc75fbb7b48ff26e8
Lucas Vinicius / INTELLEART <147888270+20026518-5@users.noreply.github.com>
2026-01-23 20:06:34 -0300

    Integrate routing and context providers in App
    Refactor App component to include routing and context providers.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import RoutesApp from './routes';
+import AuthProvider from './contexts/auth';
+import ThemeProvider from './contexts/ThemeContext'; // <--- Importe aqui
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <ThemeProvider> {/* <--- Envolva as rotas aqui */}
+          <ToastContainer autoClose={3000} />
+          <RoutesApp />
+        </ThemeProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode> //// tirar para n duplicar a lista de chamados
-);
+export default App;
