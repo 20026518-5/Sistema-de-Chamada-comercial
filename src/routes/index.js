8df423e438fffb6fddd8cef7e584dcc8f59bb139
Lucas Vinicius / INTELLEART <147888270+20026518-5@users.noreply.github.com>
2026-01-23 20:13:15 -0300

    Refactor routes to use updated component names
    Updated route component imports and paths for Customers, New, and Settings.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
-/* src/routes/index.js */
 import { Routes, Route } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
-import Servidores from '../pages/Customers'; // O componente que editamos chama Servidores
+import Customers from '../pages/Customers';
+import New from '../pages/New';
+import Settings from '../pages/Settings'; 
 import Private from './private';
 
 function RoutesApp(){
@@ -13,12 +14,17 @@ function RoutesApp(){
     <Routes>
       <Route path="/" element={ <SignIn/> } />
       <Route path="/register" element={ <SignUp/> } />
-      
+
       <Route path="/dashboard" element={ <Private><Dashboard/></Private> } />
       <Route path="/profile" element={ <Private><Profile/></Private> } />
       
-      {/* VERIFIQUE SE ESTA LINHA EXISTE E SE O CAMINHO ESTÁ CORRETO */}
-      <Route path="/customers" element={ <Private><Servidores/></Private> } />
+      {/* CORREÇÃO: Alterado de /servidores para /customers para o botão funcionar */}
+      <Route path="/customers" element={ <Private><Customers/></Private> } />
+      
+      <Route path="/new" element={ <Private><New/></Private> } />
+      <Route path="/new/:id" element={ <Private><New/></Private> } />
+      
+      <Route path="/settings" element={ <Private><Settings/></Private> } />
     </Routes>
   )
 }
