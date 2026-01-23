6f15c1cf5a4ad273b6312d5c3bf98fa114dd789a
Lucas Vinicius / INTELLEART <147888270+20026518-5@users.noreply.github.com>
2026-01-23 20:12:52 -0300

    Wrap RoutesApp with ThemeProvider in App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { BrowserRouter } from "react-router-dom";
-import AuthProvider from "./contexts/auth";
+import { BrowserRouter } from 'react-router-dom';
 import RoutesApp from './routes';
+import AuthProvider from './contexts/auth';
+import ThemeProvider from './contexts/ThemeContext'; // Importante: verifique se o caminho está correto
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
-    <AuthProvider>
-      <ToastContainer autoClose={3000}/>
-      <RoutesApp />
-    </AuthProvider>
+      <AuthProvider>
+        <ThemeProvider> {/* O ThemeProvider deve ficar AQUI */}
+          <ToastContainer autoClose={3000} />
+          <RoutesApp />
+        </ThemeProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
